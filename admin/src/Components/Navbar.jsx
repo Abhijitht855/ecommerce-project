ac8fe1ed46d6dc73e9c1edaf29a1a941f742d702
abhijith <abhijitht855@gmail.com>
2024-11-29 13:00:57 +0530

    new

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -4,7 +4,12 @@ import { assets } from '../assets/assets'
 const Navbar = ({setToken}) => {
   return (
     <div className='flex justify-between items-center py-2 px-[4%]'>
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+        {/* <img className='w-[max(10%,80px)]' src={assets.logo} alt="" /> */}
+        <Link to='/'>
+  <h1 className='text-[#2b2b2b] text-3xl font-bold tracking-widest sm:text-4xl font-serif'>
+    Glamzy
+  </h1>
+</Link>
         <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
