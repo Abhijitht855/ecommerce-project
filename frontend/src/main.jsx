c8093271e4a1a851df267f431433a01cc4e90071
abhijith <abhijitht855@gmail.com>
2025-03-18 11:42:25 +0530

    changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./Context/ShopContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter future={{ v7_startTransition: true }}>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
